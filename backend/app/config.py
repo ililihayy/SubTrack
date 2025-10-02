0238888062fdf8f99bfedbc8c597c096698e9546
ililihayy <lilipushkar15@gmail.com>
2025-10-02 23:42:01 +0300

    fix: add ports to env

--- backend/app/config.py
+++ backend/app/config.py
@@ -3,7 +3,10 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL", f"sqlite:///app.db")
+DATABASE_URL = os.getenv("DATABASE_URL", f"sqlite:////data/app.db")
+
+API_PORT: int = 8000
+API_HOST: str = "0.0.0.0"
 
 RANDOM_DATA_API_BASE = os.getenv(
     "RANDOM_DATA_API_BASE", "https://random-data-api.com/api/v2"
